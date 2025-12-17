ff78f317982529196dcd0e6fd5c17f8c23ac49dc
Debajit91 <debajitroy544@gmail.com>
2025-12-17 19:17:28 +0600

    post req created with using body parser

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,20 @@ import express, { Request, Response } from 'express'
 const app = express()
 const port = 5000
 
+// parser
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello Next Level Developers!')
+})
+
+app.post('/', (req, res)=>{
+  console.log(req.body);
+
+  res.status(201).json({
+    success: true,
+    message: "API is working"
+  })
 })
 
 app.listen(port, () => {
