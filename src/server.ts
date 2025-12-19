7e69cf289c2da3785ddf5d33bf02d4e5b303f298
Debajit91 <debajitroy544@gmail.com>
2025-12-19 23:23:08 +0600

    db config seperated into db.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express'
-import {Pool} from 'pg';
 import config from './config';
+import initDB, { pool } from './config/db';
 
 
 
@@ -9,39 +9,8 @@ import config from './config';
 const app = express()
 const port = config.port
 
-// DB
-const pool = new Pool({
-  connectionString: `${config.connection_str}`
-})
 
-const initDB = async() =>{
-  await pool.query(`
-      CREATE TABLE IF NOT EXISTS users(
-      id SERIAL PRIMARY KEY,
-      name VARCHAR(100) NOT NULL,
-      email VARCHAR(150) UNIQUE NOT NULL,
-      age INT,
-      phone VARCHAR(15),
-      address TEXT,
-      created_at TIMESTAMP DEFAULT NOW(),
-      updated_at TIMESTAMP DEFAULT NOW()
-      )
-    `)
-
-    await pool.query(`
-          CREATE TABLE IF NOT EXISTS todos(
-          id SERIAL PRIMARY KEY,
-          user_id INT REFERENCES users(id) ON DELETE CASCADE,
-          title VARCHAR(200) NOT NULL,
-          description TEXT,
-          completed BOOLEAN DEFAULT false,
-          due_date DATE,
-          created_at TIMESTAMP DEFAULT NOW(),
-          updated_at TIMESTAMP DEFAULT NOW()
-          )
-      `)
-}
-
+// initializing DB
 initDB();
 
 // parser
