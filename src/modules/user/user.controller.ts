c15f4a499bb5d9c42e013ec297a50e9c11cdcd47
Debajit91 <debajitroy544@gmail.com>
2025-12-20 00:16:50 +0600

    routes in routes and req, res in controller separated for post and get all users

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from "express";
+import { pool } from "../../config/db";
+
+const createUser = async (req:Request, res: Response)=>{
+  const {name, email} = req.body;
+
+  try {
+    const result = await pool.query(`INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`, [name, email]);
+
+   return res.status(201).json({
+      success: true,
+      message: "Data Inserted Successfully",
+      data: result.rows[0]
+    })
+    
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    })
+  }
+
+}
+
+const getUsers = async (req: Request, res: Response) =>{
+  try {
+    const result = await pool.query(`SELECT * FROM users`)
+    res.status(201).json({
+      success: true,
+      message: "Users Retrieved Successfully",
+      data: result.rows
+    })
+  } catch (error:any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      details: error
+    })
+  }
+}
+
+export const userController = {
+    createUser,
+    getUsers
+}
--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,47 +1,14 @@
 import { Request, Response, Router } from "express";
 import { pool } from "../../config/db";
+import { userController } from "./user.controller";
 
 const router = Router();
 
 // localhost:5000/users/ -> /
 // post method
-router.post('/', async (req:Request, res: Response)=>{
-  const {name, email} = req.body;
-
-  try {
-    const result = await pool.query(`INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`, [name, email]);
-
-   return res.status(201).json({
-      success: true,
-      message: "Data Inserted Successfully",
-      data: result.rows[0]
-    })
-    
-  } catch (error: any) {
-    res.status(500).json({
-      success: false,
-      message: error.message
-    })
-  }
-
-});
+router.post('/', userController.createUser);
 
 // get all users
-router.get('/', async (req: Request, res: Response) =>{
-  try {
-    const result = await pool.query(`SELECT * FROM users`)
-    res.status(201).json({
-      success: true,
-      message: "Users Retrieved Successfully",
-      data: result.rows
-    })
-  } catch (error:any) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-      details: error
-    })
-  }
-})
+router.get('/', userController.getUsers)
 
 export const userRoutes = router;
